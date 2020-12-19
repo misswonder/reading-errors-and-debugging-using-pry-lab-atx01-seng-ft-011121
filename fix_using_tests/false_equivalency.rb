acd1b7bf70c16c6e03372b1a9edab0e6dd80e460
Yvonne Chen <yvonnechen1991@gmail.com>
2020-12-19 07:07:39 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,5 +1,7 @@
 # don't forget to add: require 'pry'
 
+require 'pry'
+
 def get_user_input
   gets.chomp
 end
@@ -11,9 +13,10 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
+    binding.pry
+  if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  else
      "HAM HAM HAM IN MY TUMMY"
   end
 end
